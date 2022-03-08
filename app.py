26f0927f62a311cc9cab39481f38ed097894da67
ike <iua6@njit.edu>
2022-03-07 23:46:03 -0500

    pushing commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#add pages here
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/page1')
+def page1():
+    return render_template('page1.html')
+
+@app.route('/page2')
+def page2():
+    return render_template('page2.html')
+
+@app.route('/page3')
+def page3():
+    return render_template('page3.html')
+
+@app.route('/page4')
+def page4():
+    return render_template('page4.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
